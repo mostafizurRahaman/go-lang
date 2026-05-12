2890c81d6ce4f9494e6e39c6a583d24a1d16c844
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2026-05-12 21:30:11 +0600

    feat(simple_values): Print simple values

--- personal_practice/simple_values/main.go
+++ personal_practice/simple_values/main.go
@@ -1,10 +1,24 @@
-package main 
+package main
 
-func main(){ 
+import "fmt"
 
-	
+func main() {
 
+	// print string:
+	fmt.Println("I was a student of cumilla polytechnic institute!")
 
+	// print interger number:
+	fmt.Println(3)
+	fmt.Println(2.0)
 
+	// Do Arethmatic operation:
+	fmt.Println(3 + 3)
+	fmt.Println(3.0 + 3.2)
+
+	// do division
+	fmt.Println(3.0 / 2.0)
+	fmt.Println(2.0 / 3.0)
+	fmt.Println(true)
+	fmt.Println(false)
 
 }
