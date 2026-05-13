1a20ad9a606e5c2f384c4550c9f404e132f790bf
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2026-05-13 16:03:46 +0600

    feat(variadic): a variadic has been completed

---
+++ personal_practice/veriadics/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+// ! 1. For getting multiple:
+func logger(logType string, a ...interface{}) {
+	fmt.Print(logType)
+
+	for _, v := range a {
+		fmt.Printf(" %v ", v)
+	}
+	fmt.Println()
+}
+
+// ! 2. Create antoher one variadic function which will get n number of parameters and return sum of them.
+
+func sumUp(n ...int) int {
+	sum := 0
+
+	for _, v := range n {
+		sum = sum + v
+	}
+
+	return sum
+}
+
+func main() {
+	fmt.Println("Veriadic functions")
+
+	logger("INFO", 1, 2, 3, 4, 5, "MOstafizur", "Osman goni")
+	logger("INFO", 1, 2, 3, 4, 5, "MOstafizur", "Osman goni")
+	logger("INFO", 1, 2, 3, 4, 5, "MOstafizur", "Osman goni")
+	logger("INFO", 1, 2, 3, 4, 5, "MOstafizur", "Osman goni")
+	logger("INFO", 1, 2, 3, 4, 5, "MOstafizur", "Osman goni")
+
+	fmt.Println("Print", sumUp(20, 20, 20, 10))
+
+}
