8d02cc0359554ca71af4b3418c71580f3d56f4f5
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2026-05-15 23:00:42 +0600

    feat(interfaces): Interface completed

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,16 +2,25 @@ package main
 
 import "fmt"
 
+type paymenter interface {
+	pay(amount int)
+}
+
 // ! Create a struct for payment implemention
+// type payment struct {
+// 	razorpay razorpay
+// 	stripe stripe
+// }
+
 type payment struct {
-	// razorpay razorpay
-	stripe stripe
+	// gateway stripe
+	gateway paymenter
 }
 
 // ! Create make payment function for payment :
 func (p payment) makePayment(amount int) {
 	// p.razorpay.pay(amount)
-	p.stripe.pay(amount)
+	p.gateway.pay(amount)
 }
 
 // ! Create another struct to pay with razor pay:
@@ -32,18 +41,32 @@ func (s stripe) pay(amount int) {
 	fmt.Println("Amount has been paid using stripe", amount)
 }
 
+// Type fack payment gateway:
+
+type fakeGateway struct {
+}
+
+func (f fakeGateway) pay(amount int) {
+	fmt.Println("Now making payment with fake gateway", amount)
+}
+
 func main() {
 
 	// ! create an instance for razorpay first:
 	// razorpayPaymentSettings := razorpay{}
 
 	// ! Create an interface for stripe:
-	stripePaymentSettings := stripe{}
+	// stripePaymentSettings := stripe{}
+
+	// ! Create an instance for fakeGateway
+	fakeGatewayPaymentSettings := fakeGateway{}
 
 	// ! Create one instance  for payment
 	paymentSystem := payment{
 		// razorpay: razorpayPaymentSettings,
-		stripe: stripePaymentSettings,
+		// stripe: stripePaymentSettings,
+		// gateway: stripePaymentSettings,
+		gateway: fakeGatewayPaymentSettings,
 	}
 
 	paymentSystem.makePayment(200)
